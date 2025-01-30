74307aab41a2155c0d0e20666a2b9ca5d9219d28
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-01-30 11:55:36 +0700

    Import matCard, matButton

--- src/app/components/employee/employee.component.ts
+++ src/app/components/employee/employee.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
-
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'app-employee',
-  imports: [],
+  imports: [MatCardModule, MatButtonModule],
   templateUrl: './employee.component.html',
   styleUrl: './employee.component.css'
 })
 export class EmployeeComponent {
+  addemployee() {
 
+  }
 }
