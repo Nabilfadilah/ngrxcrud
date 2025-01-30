46a90d123f98944b7740c90a5daac49a5dbc1207
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-01-30 14:59:43 +0700

    Import module dan menjalankan fungsi get all product dan open dialog add employee

--- src/app/components/employee/employee.component.ts
+++ src/app/components/employee/employee.component.ts
@@ -1,21 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { AddEmployeeComponent } from '../add-employee/add-employee.component';
+import { Employee } from '../../model/Employee';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { EmployeeService } from '../../service/employee.service';
+import { Subscription } from 'rxjs';
+import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-employee',
-  imports: [MatCardModule, MatButtonModule, MatDialogModule],
+  imports: [MatCardModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatTableModule,
+    CommonModule
+  ],
   templateUrl: './employee.component.html',
   styleUrl: './employee.component.css'
 })
-export class EmployeeComponent {
+export class EmployeeComponent implements OnInit {
 
-  constructor(private dialog: MatDialog) {
+  empList: Employee[] = [];
+  dataSource!: MatTableDataSource<Employee>;
+  displayedColumns: string[] = ['id', 'name', 'role', 'doj', 'salary', 'action']
+  subscription = new Subscription();
+
+  constructor(private dialog: MatDialog, private service: EmployeeService) {
 
   }
 
+  ngOnInit(): void {
+    this.GetallEmployee();
+  }
+
+
+
+  GetallEmployee() {
+    let sub = this.service.GetAll().subscribe(item => {
+      this.empList = item;
+      this.dataSource = new MatTableDataSource(this.empList);
+    })
+    this.subscription.add(sub);
+  }
+
   addemployee() {
-    this.dialog.open(AddEmployeeComponent)
+    this.dialog.open(AddEmployeeComponent, {
+      width: '50%',
+      exitAnimationDuration: '1000ms',
+      enterAnimationDuration: '1000ms'
+    }).afterClosed().subscribe(o => {
+      this.GetallEmployee();
+    })
+
   }
 }
