30974576f2554ce8f069de720829d7753e3f9d11
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-02-01 11:04:24 +0700

    Menambah aksi delete yang di gunakan di store

--- src/app/store/EmployeeAction.ts
+++ src/app/store/EmployeeAction.ts
@@ -6,6 +6,14 @@ export const LOAD_EMPLOYEE = 'employee getall'
 export const LOAD_EMPLOYEE_SUCCESS = 'employee getall suc'
 export const LOAD_EMPLOYEE_FAIL = 'employee getall fail'
 
+export const DELETE_EMPLOYEE = '[employee] delete'
+export const DELETE_EMPLOYEE_SUCCESS = '[employee] delete succ'
+
 export const loadEmployee = createAction(LOAD_EMPLOYEE) // Aksi untuk mengambil data karyawan
 export const loadEmployeeSuc = createAction(LOAD_EMPLOYEE_SUCCESS, props<{ list: Employee[] }>()) // Aksi yang dipanggil jika pengambilan data sukses
 export const loadEmployeeFail = createAction(LOAD_EMPLOYEE_FAIL, props<{ errMsg: string }>()) // Aksi yang dipanggil jika pengambilan data gagal
+
+export const deleteEmployee = createAction(DELETE_EMPLOYEE, props<{ empId: number }>()) // Aksi untuk mengambil data karyawan berdasasrkan empId
+export const deleteEmployeeSuc = createAction(DELETE_EMPLOYEE_SUCCESS, props<{ empId: number }>()) // Aksi yang dipanggil jika pengambilan data sukses
+
+export const emptyAction = createAction('empty')
