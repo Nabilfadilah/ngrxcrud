16223a5ea76b85f65ace9ed262a4361d6c1a8214
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-02-01 21:55:55 +0700

    Menambah property empobj

--- src/app/store/EmployeeModel.ts
+++ src/app/store/EmployeeModel.ts
@@ -3,4 +3,5 @@ import { Employee } from "../model/Employee";
 export interface EmployeeModel {
     list: Employee[], // Menyimpan daftar karyawan dalam bentuk array
     errormessage: string // Menyimpan pesan kesalahan jika terjadi error
+    empobj: Employee
 }
