737896859fac5a33dcab4f223382e96c53edafcd
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-02-01 11:07:41 +0700

    Membuat deleteEmploye yang menangani side effect ketika delete dan memunculkan alert

--- src/app/store/EmployeeEffects.ts
+++ src/app/store/EmployeeEffects.ts
@@ -1,15 +1,18 @@
 import { inject, Injectable } from "@angular/core";
 import { EmployeeService } from "../service/employee.service";
 import { Actions, createEffect, ofType } from "@ngrx/effects";
-import { loadEmployee, loadEmployeeFail, loadEmployeeSuc } from "./EmployeeAction";
-import { catchError, exhaustMap, map, of } from "rxjs";
+import { deleteEmployee, deleteEmployeeSuc, emptyAction, loadEmployee, loadEmployeeFail, loadEmployeeSuc } from "./EmployeeAction";
+import { catchError, exhaustMap, map, of, switchMap } from "rxjs";
+import { ToastrService } from "ngx-toastr";
 
 @Injectable()
 export class empEffect {
 
     actions$ = inject(Actions); // Observable yang berisi aksi-aksi yang terjadi di store
     service = inject(EmployeeService); // Service untuk memanggil API
+    toastr = inject(ToastrService); // mengambil alert
 
+    // mengambil data
     _loadEmployee = createEffect(() =>
         this.actions$.pipe(
             ofType(loadEmployee), // Menunggu aksi `loadEmployee`
@@ -23,4 +26,31 @@ export class empEffect {
             })
         )
     )
+
+    // delete
+    _deleteEmployee = createEffect(() =>
+        this.actions$.pipe(
+            ofType(deleteEmployee), // Menunggu aksi `deleteEmployeeSuc`
+            switchMap((action) => {
+                return this.service.Delete(action.empId).pipe( // Mengapus data keryawan
+                    switchMap((data) => {
+                        return of(deleteEmployeeSuc({ empId: action.empId }), // Mengirim aksi `deleteEmployeeSuc` dengan `empId` yang dihapus
+                            this.showAlert('Deleted Successfully.', 'pass')
+                        )
+                    }), // Jika berhasil, kirim aksi `deleteEmployeeSuc`
+                    catchError((err) => of(this.showAlert(err.message, 'fail'))) // Jika gagal, tampilkan alert
+                )
+            })
+        )
+    )
+
+    // menampilkan alert
+    showAlert(message: string, response: string) {
+        if (response == 'pass') {
+            this.toastr.success(message);
+        } else {
+            this.toastr.error(message);
+        }
+        return emptyAction();
+    }
 }
