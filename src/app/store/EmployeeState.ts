0d8d62b91c0cccdd3a8fa1dc0e6ce598bb7f9d4d
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-02-01 21:57:01 +0700

    Menambah state property empobj dan memberikan objek properti

--- src/app/store/EmployeeState.ts
+++ src/app/store/EmployeeState.ts
@@ -2,5 +2,12 @@ import { EmployeeModel } from "./EmployeeModel";
 
 export const employeeState: EmployeeModel = {
     list: [], // Awalnya list kosong (tidak ada data karyawan)
-    errormessage: "" // Tidak ada error message saat awal
+    errormessage: "", // Tidak ada error message saat awal
+    empobj: {
+        id: 0,
+        name: "",
+        doj: new Date(),
+        role: "",
+        salary: 0
+    }
 }
