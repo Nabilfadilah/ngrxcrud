44afbab9a2ed35bc827bb2a9cac1277d4807846e
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-02-01 10:11:13 +0700

    Menentukan nilai awal untuk state employee

---
+++ src/app/store/EmployeeState.ts
@@ -0,0 +1,6 @@
+import { EmployeeModel } from "./EmployeeModel";
+
+export const employeeState: EmployeeModel = {
+    list: [], // Awalnya list kosong (tidak ada data karyawan)
+    errormessage: "" // Tidak ada error message saat awal
+}
